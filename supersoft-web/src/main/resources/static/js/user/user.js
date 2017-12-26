4afb3a7e73bc46e684bcd94f001af81693d6bc00
张建波 <k963@vip.qq.com>
2017-12-26 14:11:18 +0800

    分页

--- supersoft-web/src/main/resources/static/js/user/user.js
+++ supersoft-web/src/main/resources/static/js/user/user.js
@@ -1,12 +1,3 @@
-/**
- * @Author: Larry  2017-04-16 17:20:56
- *+----------------------------------------------------------------------
- *| LarryBlogCMS [ LarryCMS网站内容管理系统 ]
- *| Copyright (c) 2016-2017 http://www.larrycms.com All rights reserved.
- *| Version 1.09
- *| <313492783@qq.com>
- *+----------------------------------------------------------------------
- */
 layui.use(['jquery','layer','form','table'],function(){
     'use strict';
     var $ = layui.jquery
