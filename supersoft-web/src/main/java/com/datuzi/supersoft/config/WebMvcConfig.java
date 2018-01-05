1ebdd5f6fcb9bab0973b44e28f2bbdcabef478b9
张建波 <k963@vip.qq.com>
2018-01-05 12:29:24 +0800

    404

--- supersoft-web/src/main/java/com/datuzi/supersoft/config/WebMvcConfig.java
+++ supersoft-web/src/main/java/com/datuzi/supersoft/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import com.datuzi.supersoft.interceptor.ApiInterceptor;
 import com.datuzi.supersoft.interceptor.BaseInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -28,20 +29,21 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(baseInterceptor).addPathPatterns("/**").excludePathPatterns("/toLogin","/login","/img/code");
+        registry.addInterceptor(baseInterceptor).addPathPatterns("/**")
+                .excludePathPatterns("/","/docs","/toLogin","/login","/img/code","/404","/401","/500");
         registry.addInterceptor(apiInterceptor).addPathPatterns("/api/**");
         super.addInterceptors(registry);
     }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController( "/" ).setViewName( "redirect:/toLogin" );
+        registry.addRedirectViewController("/docs","/swagger-ui.html");
         registry.addViewController("/404").setViewName("common/404");
         registry.addViewController("/401").setViewName("common/401");
         registry.addViewController("/500").setViewName("common/500");
-        registry.addViewController("/wc/grzx").setViewName("common/grzx");
-        registry.addViewController("/users/pwd").setViewName("update-pwd");
-        registry.addRedirectViewController("/docs","/swagger-ui.html");
-        super.addViewControllers(registry);
+        registry.setOrder( Ordered.HIGHEST_PRECEDENCE );
+        super.addViewControllers( registry );
     }
 
 }
